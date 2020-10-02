0c32f43fdc7ca6e3b433fbbb0a84cd68901c5d19
DESKTOP-FF2H0VE\oghkr <oyfkr@naver.com>
2020-10-03 01:18:36 +0900

    완주하지 못한 선수 찾기
    참가자 문자열과 완주자 문자열을 입력받는다.
    참가자 문자열에는 있지만 완주자 문자열에는 없는 사람을 찾는다.
    Array.sort를 이용하여 두 문자열을 오름차순으로 정렬시킨다.
    for문을 이용하여 각 문자열에 같은 인덱스가 같은지 비교한 뒤
    다르면 answer 문자열에그 인덱스 값을 입력한다.
    answer 값을 리턴한다.

--- programmers/player_not_finished.java
+++ programmers/player_not_finished.java
@@ -1,23 +1,30 @@
+package programmers;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class player_not_finished {
     public static void main(String args[]){
         String[] participant = {"a", "b", "c", "d", "e"};
-        String[] completion = {"a", "c", "b", "e"};
+        String[] completion = {"a", "e", "d","b"};
 
         System.out.println(solution(participant, completion));
     }
     public static String solution(String[] participant, String[] completion){
         String answer = "";
+
         Arrays.sort(participant);
         Arrays.sort(completion);
-
-        for(int i = 0; i < completion.length; i++){
+        boolean flag = true;
+        for(int i = 0; i < completion.length;  i++){
             if(!participant[i].equals(completion[i])){
                 answer = participant[i];
+                flag = false;
+                break;
             }
-            answer = participant[completion.length];
+        }
+        if(flag == true){
+            answer = participant[participant.length-1];
         }
 
         return answer;
