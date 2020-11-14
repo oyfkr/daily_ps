a9515ee147479fdbb57285be92a6434325127834
DESKTOP-FF2H0VE\oghkr <oyfkr@naver.com>
2020-11-14 19:55:41 +0900

    k 번째 수 찾기

--- programmers/k_number.java
+++ programmers/k_number.java
@@ -1,15 +1,15 @@
 package programmers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class k_number {
     public static void main(String args[]){
         int[] array = {1,5,2,6,3,7,4};
-        int[][] commands ={{2,5,3},{4,4,1},{1,7,3}};
+        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
         System.out.println(Arrays.toString(solution(array,commands)));
     }
-    public static int[] solution(int[] array, int[][] commands){
-
+    public static int[] solution(int []array, int[][] commands){
         int answer[] = new int[commands.length];
         for(int i = 0; i < commands.length; i++) {
             int fir = commands[i][0] - 1;
