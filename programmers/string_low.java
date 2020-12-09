b4b8bee8c0efa86275cfc5adbd739ef7aeb46b4c
DESKTOP-FF2H0VE\oghkr <oyfkr@naver.com>
2020-12-09 14:05:24 +0900

    프로그래머스 문자열 내림차순으로 배치하기
    받은 문자열을 charat을 이용하고 강제 형변환을 시켜
    integer배열에 아스키코드로 넣는다.
    integer배열을 내림차순으로 정리한후 answer 문자열에 한글자씩
    문자로 강제형변환을 한 채로 넣는다. 출력하면 내림차순으로
    정리된것을 볼수 있다.

---
+++ programmers/string_low.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class string_low {
+    public static void main(String args[]){
+        String s = "Zbcdefg";
+        System.out.println(solution(s));
+    }
+    public static String solution(String s){
+        String answer ="";
+        Integer[] arr = new Integer[s.length()];
+        for(int i = 0; i<s.length();i++){
+            arr[i] = (int)s.charAt(i);
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println(arr[1]);
+        for(int i = 0; i< arr.length;i++){
+            answer +=Character.toString(arr[i]);
+        }
+        return answer;
+    }
+}
