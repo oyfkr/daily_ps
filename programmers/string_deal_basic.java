2c72825d624ebb7dd7650db2a65ef4ce22240596
DESKTOP-FF2H0VE\oghkr <oyfkr@naver.com>
2020-12-09 16:26:57 +0900

    프로그래머스 문자열 다루기 기본
    문자열을 받아서 그 문자열안에 숫자만 있는지 찾는 문제
    문자열을 charat을 이용하여 문자열 하나하나의 아스키 값을 얻고
    그걸로 0~10사이의 아스키 코드값이 아닌 값이 있다면 for문을 break하고
    모든 수가 0~10사이의 아스키 코드값이면 answer에 true를 넣는다.

---
+++ programmers/string_deal_basic.java
@@ -0,0 +1,34 @@
+package programmers;
+
+public class string_deal_basic {
+    public static void main(String args[]) {
+        String s = "11234";
+        System.out.println(solution(s));
+    }
+
+    public static boolean solution(String s) {
+        boolean answer = true;
+        if (s.length() == 4 || s.length() == 6) {
+            int[] tmp = new int[s.length()];
+            for (int i = 0; i < s.length(); i++) {
+                tmp[i] = s.charAt(i);
+            }
+
+            for (int i = 0; i < tmp.length; i++) {
+                if (tmp[i] >= 48 && tmp[i] <= 57) {
+                    answer = true;
+
+                } else {
+                    answer = false;
+                    break;
+                }
+                ;
+            }
+
+            return answer;
+        }
+        else answer = false;
+        return answer;
+    }
+
+}
