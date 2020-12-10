961080e451ec24d095e355d8af80acbc05d472b4
DESKTOP-FF2H0VE\oghkr <oyfkr@naver.com>
2020-12-11 01:39:18 +0900

    프로그래머스 서울에서 김서방 찾기
    for문을 이용하여 Kim이 들어있는 i를 찾아서 출력한다.

---
+++ programmers/findkim_inseoul.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class findkim_inseoul {
+    public static void main(String args[]){
+        String[] seoul = {"Jane","asd","Kim"};
+        System.out.println(solution(seoul));
+    }
+    public static String solution(String[] seoul){
+        String answer = "";
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 "+ i +"에 있다";
+            }
+        }
+        return answer;
+    }
+}
