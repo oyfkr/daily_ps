2d51274bbd2bd2ce51a859e9c1bd55663be7f197
oyfkr <oyfkr@naver.com>
2020-12-31 16:51:56 +0900

    프로그래머스 2단계 문자열 압축

---
+++ programmers_2/string_compression.java
@@ -0,0 +1,52 @@
+package programmers_2;
+
+public class string_compression {
+    public static void main(String args[]){
+        String s = "aabbaccc";
+        string_compression a = new string_compression();
+        System.out.println(a.solution(s));
+    }
+    public int solution(String s) {
+        int answer = s.length();
+        int halflength = s.length() / 2;
+
+        //i 만큼 검사
+        for(int i = 1; i <= halflength;i++){
+            int start = 0;
+            int end = start + i;
+            int cnt = 1;
+            int compression = 0;
+            String cur = s.substring(start,end);
+            String next ="";
+
+            start += i;
+            end += i;
+
+            while(end <= s.length()){
+                next = s.substring(start,end);
+                if(!cur.equals(next)){
+                    if(cnt>1){
+                        compression += (int)(Math.log10(cnt)+1);
+                    }
+                    compression += cur.length();
+                    cnt = 0;
+                    cur = next;
+                }
+                cnt++;
+                start +=i;
+                end += i;
+            }
+            end -= i;
+            compression += s.substring(end).length();
+            if(cnt > 1){
+                compression += (int)(Math.log10(cnt)+1);;
+            }
+            compression += cur.length();
+
+            if(answer > compression){
+                answer = compression;
+            }
+        }
+        return answer;
+    }
+}
