170e2a9cc0a63cb6adecdc6ec42c783cdabe7b42
oyfkr <oyfkr@naver.com>
2021-01-05 14:47:35 +0900

    프로그래머스 2단계 구명보트

---
+++ programmers_2/lifrboat.java
@@ -0,0 +1,22 @@
+package programmers_2;
+
+import java.util.Arrays;
+
+public class lifrboat {
+    public static void main(String[] args) {
+        int[] people ={70,50,80,50};
+        int limit = 100;
+        lifrboat s = new lifrboat();
+        System.out.println(s.solution(people,limit));
+    }
+    public int solution(int[] people, int limit){
+        int answer = 0;
+        int cur = people.length-1;
+        Arrays.sort(people);
+        for(int i=0; i<=cur; i++,answer++) {
+            while (cur > i && people[i] + people[cur--] > limit)
+                answer++;
+        }
+        return answer;
+    }
+}
