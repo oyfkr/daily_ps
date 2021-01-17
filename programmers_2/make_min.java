d4cef9cdc9be0373f5f837accae58b8e13dfa16b
oyfkr <oyfkr@naver.com>
2021-01-18 00:36:02 +0900

    프로그래머스 2단계 피보나치 수

---
+++ programmers_2/make_min.java
@@ -0,0 +1,22 @@
+package programmers_2;
+
+import java.util.Arrays;
+
+public class make_min {
+    public static void main(String[] args) {
+        int []A = {1,4,2};
+        int []B = {5,4,4};
+        make_min s = new make_min();
+        System.out.println(s.solution(A,B));
+    }
+    public int solution(int[]A, int[]B){
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i = 0; i<A.length;i++){
+            answer += A[i] * B[B.length-1-i];
+        }
+
+        return answer;
+    }
+}
